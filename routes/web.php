a1198912b3614883b87dcdbe03f4df600725c6e7
Mike Tiano <mike@Shelby.local>
2018-09-19 14:51:38 -0600

    Sync update

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,12 @@ Route::get('auth/{provider}', 'Auth\AuthController@redirectToProvider');
 
 Route::get('auth/{provider}/callback', 'Auth\AuthController@handleProviderCallback');
 
+// Password Reset Routes...
+//Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+//Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+//Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+//Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
 Route::resource('links', 'LinksController');
 Route::resource('tags', 'TagsController');
 
