ab3450a1547420be199f8fedda9078346eb10954
Mike Tiano <mike@Shelby.local>
2018-10-10 04:49:04 -0600

    Redesigned Navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,12 +1,13 @@
 <header>
     <!-- Fixed navbar -->
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <nav class="navbar main_nav navbar-expand-md navbar-light fixed-top bg-light">
+        <div class="inner container">
         <a class="navbar-brand" href="/">SITE LOGO</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav mr-auto justify-content-end">
                 <li class="nav-item active">
                     <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
                 </li>
@@ -22,11 +23,9 @@
                     <a class="nav-link" href="/roles">Roles</a>
                 </li>
                 @endrole
-            </ul>
 
-            @if (Auth::check())
+                @if (Auth::check())
 
-            <ul class="navbar-nav ml-auto">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                         {{ Auth::user()->name }}
@@ -37,21 +36,21 @@
                         <a class="dropdown-item" href="/logout">Logout</a>
                     </div>
                 </li>
-            </ul>
 
-            @else
+                @else
 
-            <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="/login">Login</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/register">Register</a>
                 </li>
-            </ul>            
 
-            @endif
+                @endif
+
+            </ul>
 
         </div>
+    </div><!-- .inner -->
     </nav>
 </header>
