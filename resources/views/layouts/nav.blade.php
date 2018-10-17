0b7bcd44d1023a7ad8b425a984961c71d6288841
Celso Malacas Jr <fortislogo@hotmail.com>
2018-10-17 13:43:13 +0800

    fix errors

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,65 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<header>
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="/">SITE LOGO</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-                </li>
-
-                @role('admin')
-                <li class="nav-item">
-                    <a class="nav-link" href="/categories">Category</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/users">Users</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/roles">Roles</a>
-                </li>
-                @endrole
-            </ul>
-
-            @if (Auth::check())
-
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                        {{ Auth::user()->name }}
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="/stacks/create">Create a Stack</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="/logout">Logout</a>
-                    </div>
-                </li>
-            </ul>
-
-            @else
-
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="/login">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/register">Register</a>
-                </li>
-            </ul>            
-
-            @endif
-
-        </div>
-    </nav>
-</header>
-=======
-=======
->>>>>>> e236547ae1352cda20a82d8e253db6ee8eb6ec3c
 <header>
     <!-- Fixed navbar -->
     <nav class="navbar main_nav navbar-expand-md navbar-light fixed-top bg-light">
@@ -136,7 +74,4 @@
     </div><!-- .inner -->
     </nav>
 </header>
-<<<<<<< HEAD
->>>>>>> a298193d466df19cca21e11c3575073676d3e7c0
-=======
->>>>>>> e236547ae1352cda20a82d8e253db6ee8eb6ec3c
+
