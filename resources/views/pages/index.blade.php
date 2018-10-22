79d93f22d9c7aade05d2d3045af4c127bde1e7c6
Celso Malacas Jr <fortislogo@hotmail.com>
2018-10-22 16:15:18 +0800

    update dashboard stack box

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,20 +1,24 @@
 @extends('layouts.master')
 
 @section('content')
+
+	<div class="dashboard">
 	
-	@include('pages.nav')
+		@include('pages.nav')
 
-	@include('stacks.following')
+		@include('stacks.following')
 
-	@include('people.following')
+		@include('people.following')
 
-	@include('stacks.mystacks')
+		@include('stacks.mystacks')
 
-	@include('stacks.recommended')
+		@include('stacks.recommended')
 
-	@include('pages.tags')
+		@include('pages.tags')
 
-	@include('pages.parking')
+		@include('pages.parking')
+
+	</div>	
 
 		
 @endsection
