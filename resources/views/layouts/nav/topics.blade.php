571ef27ed1211dd817971f425b5af59c226c9ab6
Mike Tiano <mike@Shelby.local>
2018-10-23 10:50:42 -0600

    Partial Header Design for sync

---
+++ resources/views/layouts/nav/topics.blade.php
@@ -0,0 +1,13 @@
+<div class="topics-wrapper">
+    <ul class="nav nav-topics">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Topics</a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Topic 1</a>
+                <a class="dropdown-item" href="#">Topic 2</a>
+                <a class="dropdown-item" href="#">Topic 3</a>
+                <a class="dropdown-item" href="#">Topic 4</a>
+            </div>
+        </li>
+    </ul>
+</div>
