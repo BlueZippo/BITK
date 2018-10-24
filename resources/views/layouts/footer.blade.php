05e77901adb57e3ee9f6e13f9022d04cb0bcb7d1
Celso Malacas Jr <fortislogo@hotmail.com>
2018-10-24 23:38:53 +0800

    fix $ error

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -8,7 +8,7 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
 
-    <script src="{{ asset('js/jquery-3.3.1.js') }}"></script>
+    
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 
