a7bf0b384bda8b2e2e948b064316aecb076fcdf4
Mike Tiano <michael.tiano@gmail.com>
2018-11-01 01:55:52 -0600

    Made edits on the Dashboard

--- resources/views/stacks/dashboard-box.blade.php
+++ resources/views/stacks/dashboard-box.blade.php
@@ -1,12 +1,12 @@
     <div class="inner-wrap" id="stack{{$stack['id']}}">
-      
+
 
       <div class="stack-content">
         <div class="title">
-            <a href="/stacks/{{$stack['id']}}/dashboard">{{$stack['title']}}</a>
+            <h4><a href="/stacks/{{$stack['id']}}/dashboard">{{$stack['title']}}</a></h4>
         </div>
 
-        
+
 
         <div class="author user-ctrl">
           <div class="avatar">
@@ -22,7 +22,7 @@
         </div>
 
         <div class="stack-footer">
-            <a class="btn btn-primary" href="/stacks/{{$stack['id']}}/dashboard">Start Learning</a>            
+            <a class="btn btn-primary" href="/stacks/{{$stack['id']}}/dashboard">Start Learning</a>
         </div>
       </div>
     </div>
