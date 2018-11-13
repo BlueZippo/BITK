6baf614645825096c9623d0aef48b110d2c21d16
Mike Tiano <michael.tiano@gmail.com>
2018-11-13 04:42:00 -0700

    Updated CSS for the Featured image in Edit a Stack.

--- resources/views/stacks/image.blade.php
+++ resources/views/stacks/image.blade.php
@@ -1,2 +1,2 @@
-<img width="100%" height="315" src="{{$stack->video_id}}">
- <br /><a data-toggle="modal" data-target="#youtubeModal">New Video/Image</a>
+<div class="featured-image-upload"><img width="100%" height="315" src="{{$stack->video_id}}" /></div>
+ <p class="featured-upload-btn"><a data-toggle="modal" data-target="#youtubeModal"><i class="fas fa-file-upload"></i> New Video/Image</a></p>
