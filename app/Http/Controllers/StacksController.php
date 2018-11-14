bde835c9c469a37163ff70174c8bb22166166d11
Celso Malacas Jr <fortislogo@hotmail.com>
2018-11-15 00:33:05 +0800

    update add link button

--- app/Http/Controllers/StacksController.php
+++ app/Http/Controllers/StacksController.php
@@ -920,6 +920,21 @@ class StacksController extends Controller {
 
             $data['media_id'] = $media_id;
 
+
+
+        $recents = Stack::select('id', 'title')->where('user_id', '=', auth()->id())->orderby('updated_at', 'desc')->limit(5)->get();
+
+        $options = array(
+                    'Most Recent Stacks' => $recents->pluck('title', 'id')->toArray(),
+                    'parking' => 'Parking Lot',
+                    'new' => 'Create New Stack',
+                    'My Stacks' => Stack::where('user_id', '=', auth()->id())->orderby('title')->get()->pluck('title','id')->toArray(),
+                    );
+
+       
+        $data['recents'] = $recents;
+        $data['options'] = $options;
+
             return view('stacks.edit')->with($data);
 
         }
