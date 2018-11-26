ffa1289de03b67304c9f128ee4e1a1ddc37249c8
Celso Malacas Jr <fortislogo@hotmail.com>
2018-11-27 00:32:12 +0800

    add facebook

---
+++ vendor/bariew/phpquery/phpQuery/phpQuery/DOMDocumentWrapper.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * DOMDocumentWrapper class simplifies work with DOMDocument.
+ *
+ * Know bug:
+ * - in XHTML fragments, <br /> changes to <br clear="none" />
+ *
+ * @todo check XML catalogs compatibility
+ * @author Tobiasz Cudnik <tobiasz.cudnik/gmail.com>
+ * @package phpQuery
+ */
+
+namespace App\phpQuery\DOMDocumentWrapper;
+
