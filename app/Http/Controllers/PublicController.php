ca8a56f02aa9303884085b6ccf8f4a93fafd8953
Celso Malacas Jr <fortislogo@hotmail.com>
2018-11-28 16:56:44 +0800

    update explore page for non-login user

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -76,7 +76,7 @@ class PublicController extends Controller {
 
         $sql .= " JOIN users u ON u.id = s.user_id";
 
-        $sql .= " JOIN stack_links s2 ON s2.stack_id = s.id";
+        $sql .= " JOIN links s2 ON s2.stack_id = s.id";
 
         $sql .= " LEFT JOIN stack_categories c ON c.stack_id = s.id";
 
