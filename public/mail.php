70da4b7463546cb4ef3fd6a41e20347b64bd4399
Celso Malacas Jr <fortislogo@hotmail.com>
2018-12-10 10:35:57 +0800

    test email

--- public/mail.php
+++ public/mail.php
@@ -2,12 +2,29 @@
 
 if (mail('celsomalacasjr@gmail.com', 'Test', 'Test', "From: admin@platstackt.com\r\n"))
 {
-	echo "sent";
+	echo "sent to celsomalacasjrg@gmail.com<br />";
 }
 else
 {
 	echo "not sent";
 }
 
+if (mail('cmalacas@rocketsource.co', 'Test', 'Test', "From: admin@platstackt.com\r\n"))
+{
+	echo "sent to cmalacas@rocketsource.co<br />";
+}
+else
+{
+	echo "not sent";
+}
+
+if (mail('michael.tiano@gmail.com', 'Test', 'Test', "From: admin@platstackt.com\r\n"))
+{
+	echo "sent to michael.tiano@gmail.com<br />";
+}
+else
+{
+	echo "not sent";
+}
 
 ?>
