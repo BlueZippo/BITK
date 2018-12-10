3459ffcd745d66e998f5ca636b3d188df986e342
Celso Malacas Jr <fortislogo@hotmail.com>
2018-12-10 10:44:00 +0800

    test email

--- public/mail.php
+++ public/mail.php
@@ -1,6 +1,6 @@
 <?php
 
-if (mail('celsomalacasjr@gmail.com', 'Test', 'Test', "From: admin@platstackt.com\r\n"))
+if (mail('celsomalacasjr@gmail.com', 'Test', 'Test', "From: admin@platstack.com\r\nReply-To: admin@platstack.com\r\nX-Mailer: PHP/" . phpversion()))
 {
 	echo "sent to celsomalacasjrg@gmail.com<br />";
 }
@@ -9,7 +9,7 @@ else
 	echo "not sent";
 }
 
-if (mail('cmalacas@rocketsource.co', 'Test', 'Test', "From: admin@platstackt.com\r\n"))
+if (mail('cmalacas@rocketsource.co', 'Test', 'Test', "From: admin@platstack.com\r\nReply-To: admin@platstack.com\r\nX-Mailer: PHP/" . phpversion()))
 {
 	echo "sent to cmalacas@rocketsource.co<br />";
 }
@@ -18,7 +18,7 @@ else
 	echo "not sent";
 }
 
-if (mail('michael.tiano@gmail.com', 'Test', 'Test', "From: admin@platstackt.com\r\n"))
+if (mail('michael.tiano@gmail.com', 'Test', 'Test', "From: admin@platstack.com\r\nReply-To: admin@platstack.com\r\nX-Mailer: PHP/" . phpversion()))
 {
 	echo "sent to michael.tiano@gmail.com<br />";
 }
