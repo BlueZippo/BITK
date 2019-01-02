acf6ff7015ac7fc4c26fe7a8faa1fce9508358d0
Celso Malacas Jr <fortislogo@hotmail.com>
2019-01-02 23:19:07 +0800

    remove react-js

--- resources/assets/js/components/Master.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Master extends Component {
-    render() {
-        return (
-            null
-        );
-    }
-}
--- resources/js/components/WhatsNew.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class WhatsNew extends Component {
-    render() {
-        return (
-            <div>hello, world</div>
-        );
-    }
-}
