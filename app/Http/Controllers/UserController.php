5f02f59ec324867526f68b14d899a743065f1faf
vagrant <vagrant@homestead>
2019-01-10 18:36:41 +0000

    debug users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,7 +25,7 @@ class UserController extends Controller
     public function index(Request $request)
     {
 
-        $data = User::orderBy('id','DESC');
+        $data = User::orderBy('id','DESC')->paginate(5);
 
         return view('users.index',compact('data'))
 
